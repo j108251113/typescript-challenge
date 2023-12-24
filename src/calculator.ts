c0d70045cdca74a1fcec4d235493e4439640a9f4
j108251113 <j108251113@nkust.edu.tw>
2023-12-24 09:16:47 +0000

    通過 divide 測試

--- src/calculator.ts
+++ src/calculator.ts
@@ -35,6 +35,10 @@ export function multiply(a: number, b: number): number {
  */
 export function divide(a: number, b: number): number {
     // 在此實現函式
+    if (b == 0) {
+        throw new Error("Cannot divide by zero")
+    }
+    return a / b
 }
 
 /**
