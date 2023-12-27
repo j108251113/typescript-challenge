e889e9fceb9b5fc7e078cec1e3fd86bfe32c2d46
j108251113 <j108251113@nkust.edu.tw>
2023-12-27 14:40:54 +0000

    新增不相等的測試

--- src/Scramblies.test.ts
+++ src/Scramblies.test.ts
@@ -1,8 +1,11 @@
 import { describe, it, expect } from 'vitest';
 import { scramble } from './Scramblies';
 
-describe('亂碼排列', () => {
+describe('單一字元', () => {
     it('單一字元若相等時回傳的布林值為真', () => {
         expect(scramble('a', 'a')).toBeTruthy();
+    });
+    it('單一字元若不相等時回傳的布林值為假', () => {
+        expect(scramble('a', 'b')).toBeFalsy();
     });
 });
