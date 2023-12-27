4dbefaa248c91c404b2e6bb982c0f4b58ac50202
j108251113 <j108251113@nkust.edu.tw>
2023-12-27 14:48:51 +0000

    新增兩個字元的第一個測試

--- src/Scramblies.test.ts
+++ src/Scramblies.test.ts
@@ -9,3 +9,9 @@ describe('單一字元', () => {
         expect(scramble('a', 'b')).toBeFalsy();
     });
 });
+
+describe('兩個字元', () => {
+    it('兩個字元若完全相等時回傳的布林值為真', () => {
+        expect(scramble('ab', 'ab')).toBeTruthy();
+    });
+});
