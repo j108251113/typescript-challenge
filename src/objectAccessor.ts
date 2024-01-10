4cd3b2489afd8c974737b19e86ea45e9144ca964
j108251113 <j108251113@nkust.edu.tw>
2024-01-10 19:43:14 +0000

    通過 objectAccessor 測試

--- src/objectAccessor.ts
+++ src/objectAccessor.ts
@@ -14,4 +14,12 @@
  */
 export function createObjectAccessor<T>(obj: T) {
     // 請在此處寫下你的程式碼
+    function get<K extends keyof T>(key: K) {
+        return obj[key]
+    }
+
+    function set<K extends keyof T>(key: K, value: T[K]) {
+        obj[key] = value
+    }
+    return { get, set }
 }
